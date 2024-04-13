87c8d5e1a8f5f34eb5cfd1ec3d109d1563b45cd6
kalrit718 <kalindu.rithmal.other@gmail.com>
2024-04-13 17:45:52 +0530

    feat: implement the keyboard shortcuts for commands

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,8 @@ import * as vscode from 'vscode';
 import DocumentationGenerationService from './services/DocumentationGenerationService';
 import { PerformancePortalPanel } from './panels/PerformancePortalPanel';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "docscribe" is now active!');
 
 	context.subscriptions.push(
