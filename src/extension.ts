007401bce2a90edfe2ec3a37a7c2b344889729ea
kalrit718 <kalindu.rithmal.other@gmail.com>
2024-04-18 18:38:11 +0530

    feat: implement the diagnostic service

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,13 @@ import * as vscode from 'vscode';
 import DocumentationGenerationService from './services/DocumentationGenerationService';
 import { PerformancePortalPanel } from './panels/PerformancePortalPanel';
 import { Auth0AuthenticationProvider } from './services/AuthService';
+import DiagnosticService from './services/DiagnosticService';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "docscribe" is now active!');
+
+	let diagnosticService: DiagnosticService = new DiagnosticService();
 
 	context.subscriptions.push(
 		new Auth0AuthenticationProvider(context)
@@ -15,71 +18,92 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('docscribe.login', async () => {
-			// const session = await vscode.authentication.getSession("auth0", [], { createIfNone: true });
-			let session: vscode.AuthenticationSession | undefined = await vscode.authentication.getSession("auth0", [], { createIfNone: false });
-			if (session) {
-				vscode.window.showInformationMessage(`Already logged in as ${session?.account.label}!`);
-			}
-			else {
-				session = await vscode.authentication.getSession("auth0", [], { createIfNone: true });
+			try {
+				let session: vscode.AuthenticationSession | undefined = await vscode.authentication.getSession("auth0", [], { createIfNone: false });
 				if (session) {
-					vscode.window.showInformationMessage('Sign in successful!');
-					vscode.window.showInformationMessage(`Welcome ${session?.account.label}!`);
-					console.log(session);
+					vscode.window.showInformationMessage(`Already logged in as ${session?.account.label}!`);
 				}
+				else {
+					session = await vscode.authentication.getSession("auth0", [], { createIfNone: true });
+					if (session) {
+						vscode.window.showInformationMessage('Sign in successful!');
+						vscode.window.showInformationMessage(`Welcome ${session?.account.label}!`);
+						console.log(session);
+					}
+				}
+				diagnosticService.addUsername(session?.account.id);
+			}
+			catch(e: unknown) {
+				diagnosticService.sendDiagnosticsData(e);
 			}
 		})
 	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('docscribe.openPerformancePortal', () => {
-			PerformancePortalPanel.render(context.extensionUri);
+			try {
+				PerformancePortalPanel.render(context.extensionUri);
+			}
+			catch(e: unknown) {
+				diagnosticService.sendDiagnosticsData(e);
+			}
 		})
 	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('docscribe.generateDocstring', async () => {
-			let documentationGenerationService: DocumentationGenerationService = new DocumentationGenerationService(context.extensionUri);
-			
-			let editor = vscode.window.activeTextEditor;
-
-			if (!editor) {
-				vscode.window.showErrorMessage('No active editor!');
-				return;
-			}
-			
-			let selection = editor.selection;
-			let selectedText = editor.document.getText(selection).trim();
-
-			vscode.window.withProgress({
-				location: vscode.ProgressLocation.Notification,
-				title: "DocScribe",
-				cancellable: false
-			}, async (progress: vscode.Progress<{ message: string }>, token: vscode.CancellationToken) => {
+			try {
+				let documentationGenerationService: DocumentationGenerationService = new DocumentationGenerationService(context.extensionUri);
 				
-				progress.report({ message: 'Generating the documentation reference...' });
-	
-				return documentationGenerationService.generateDocstring(selectedText)
-				.then((output: string) => {
-					editor && editor.edit((editBuilder: vscode.TextEditorEdit) => {
-							editBuilder.insert(selection.start, output);
-						});
-						vscode.window.showInformationMessage('Yay! Generated the docstring successfully!');
-				})
-				.catch((error: Error) => {
-					vscode.window.showInformationMessage(`Oops! Something went wrong :/`);
-					error.message && (error.message.trim() !== '') && vscode.window.showInformationMessage(error.message);
+				let editor = vscode.window.activeTextEditor;
+
+				if (!editor) {
+					vscode.window.showErrorMessage('No active editor!');
+					return;
+				}
+				
+				let selection = editor.selection;
+				let selectedText = editor.document.getText(selection).trim();
+
+				vscode.window.withProgress({
+					location: vscode.ProgressLocation.Notification,
+					title: "DocScribe",
+					cancellable: false
+				}, async (progress: vscode.Progress<{ message: string }>, token: vscode.CancellationToken) => {
+					
+					progress.report({ message: 'Generating the documentation reference...' });
+		
+					return documentationGenerationService.generateDocstring(selectedText)
+					.then((output: string) => {
+						editor && editor.edit((editBuilder: vscode.TextEditorEdit) => {
+								editBuilder.insert(selection.start, output);
+							});
+							vscode.window.showInformationMessage('Yay! Generated the docstring successfully!');
+					})
+					.catch((error: Error) => {
+						vscode.window.showInformationMessage(`Oops! Something went wrong :/`);
+						error.message && (error.message.trim() !== '') && vscode.window.showInformationMessage(error.message);
+						diagnosticService.sendDiagnosticsData(error.message);
+					});
 				});
-			});
+			}
+			catch(e: unknown) {
+				diagnosticService.sendDiagnosticsData(e);
+			}
 		})
 	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('docscribe.toggleONNX', () => {
-			const currentSetting = vscode.workspace.getConfiguration().get('docscribe.useONNX');
-			const newSetting = !currentSetting;
-			vscode.workspace.getConfiguration().update('docscribe.useONNX', newSetting, vscode.ConfigurationTarget.Global)
-				.then(() => vscode.window.showInformationMessage(`Changed the use of ONNX environment to '${newSetting}'`));
+			try {
+				const currentSetting = vscode.workspace.getConfiguration().get('docscribe.useONNX');
+				const newSetting = !currentSetting;
+				vscode.workspace.getConfiguration().update('docscribe.useONNX', newSetting, vscode.ConfigurationTarget.Global)
+					.then(() => vscode.window.showInformationMessage(`Changed the use of ONNX environment to '${newSetting}'`));
+			}
+			catch(e: unknown) {
+				diagnosticService.sendDiagnosticsData(e);
+			}
 		})
 	);
 }
--- src/services/ApiClientService.ts
+++ src/services/ApiClientService.ts
@@ -1,4 +1,5 @@
-import { GeneratedDocstringResponse, QueryParams } from './ApiClientService.types';
+import { DiagnosticsDataResponse, GeneratedDocstringResponse, QueryParams } from './ApiClientService.types';
+import { DiagnosticsData } from './DiagnosticService.types';
 
 export default class ApiClientService {
   private baseUrl: string | undefined;
@@ -9,30 +10,48 @@ export default class ApiClientService {
 
   public async getGeneratedDocstring(strippedContent: string): Promise<GeneratedDocstringResponse> {
     let url_ = this.baseUrl + "/generate";
-
     let queryParams: QueryParams = {
       'input_method': strippedContent
     };
-
     let queryString = Object.entries(queryParams)
       .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value.toString())}`)
       .join('&');
-
     let apiUrl = `${url_}?${queryString}`;
-
     let options_: RequestInit = {
         method: "GET",
         headers: {
           'Content-Type': 'application/json',
         }
     };
-
     let response = await fetch(apiUrl, options_);
 
     if (!response.ok) {
         throw new Error(`Failed to fetch data. Status: ${response.status}`);
     }
-
     return await response.json() as GeneratedDocstringResponse;
+  }
+
+  public async postDiagnosticsData(diagnosticsData: DiagnosticsData): Promise<DiagnosticsDataResponse> {
+    let url_ = this.baseUrl + "/add_diagnostics";
+    let queryParams: QueryParams = {
+      'username': diagnosticsData.username,
+      'data': diagnosticsData.data
+    };
+    let queryString = Object.entries(queryParams)
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value.toString())}`)
+      .join('&');
+    let apiUrl = `${url_}?${queryString}`;
+    let options_: RequestInit = {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        }
+    };
+    let response = await fetch(apiUrl, options_);
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    }
+    return await response.json() as DiagnosticsDataResponse;
   }
 }
